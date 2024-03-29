19774051241d3c672b949bfc834f282ba7be25d0
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 11:31:44 +0100

    Main page

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import {StyleSheet, View} from 'react-native';
+import {Dimensions, ImageBackground, StyleSheet, View} from 'react-native';
+
+let imageSize = {};
+let backgroundImage = require("./assets/images/img.png");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-    </View>
-  );
+
+	return (
+		<View style={styles.container}>
+			<ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {},
-});
+	container: {
+		flex: 1,
+	},
+	backgroundImage: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height,
+	},
+})
